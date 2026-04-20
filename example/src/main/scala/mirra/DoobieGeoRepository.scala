5a6a95cc45203cd99bdad2bebd65abf68aa02648
Fristi <mark@vectos.net>
2026-04-20 22:29:54 +0200

    Some changes

--- example/src/main/scala/mirra/DoobieGeoRepository.scala
+++ example/src/main/scala/mirra/DoobieGeoRepository.scala
@@ -1,12 +1,14 @@
 package mirra
 
-import doobie.*
+import doobie._
 import doobie.implicits.*
+import doobie.postgres.implicits.*
+import cats.implicits.*
 
 object DoobieGeoRepository extends GeoRepository[ConnectionIO] {
 
   def setup: ConnectionIO[Unit] =
-    fr"CREATE EXTENSION IF NOT EXISTS postgis".update.run.void
+    fr"CREATE EXTENSION IF NOT EXISTS postgis".update.run.unit
 
   def distanceSphere(lon1: Double, lat1: Double, lon2: Double, lat2: Double): ConnectionIO[Double] =
     fr"SELECT ST_DistanceSphere(ST_MakePoint($lon1, $lat1), ST_MakePoint($lon2, $lat2))"
