96bc5efbe2514676ec415a2473c05c69eb57bfa0
Fristi <mark@vectos.net>
2026-04-21 22:41:45 +0200

    Fix deploy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base: '/',
+  base: '/mirra',
   title: 'Mirra',
   description: 'Mirror-test your tagless final algebras in Scala',
   themeConfig: {
